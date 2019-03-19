905a9f77539d3a30508c9bb47573435ea094cdbd
Thomas Sullivan <sullivantm15@gmail.com>
2019-03-19 11:59:35 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports =  {
+    "reportRoot":
+    "http://beta.reports.auctionedge.com/jasperserver-pro/rest_v2/reports/",
+    "reporterPassword": "EdgeHogs%231",
+    "reporterUsername": "joeuser",
+    "startDate": "2018-12-01",
+    "endDate": "2018-12-07",
+    "reports": [
+	"public/Auctions/Postsale/SaleExpenseSummary.html",
+	"public/Auctions/Postsale/SaleExpenseSummary_1.html",
+	"public/Auctions/Postsale/SaleExpenseDetail.html",
+	"public/Auctions/Postsale/SaleExpenseDetail2.html",
+	"public/Auctions/Postsale/Buyer_Purchase_Summary_Condensed.html",
+	"public/Auctions/Postsale/PurchaseExpenseDetail2.html",
+	"public/Auctions/Postsale/Retention_Summary_Report_by_Dealer.html",
+	"public/Auctions/Postsale/Retention_Report.html",
+	"public/Auctions/Postsale/EDGE_Block_Summary_by_Group.html",
+	"public/Auctions/Postsale/Americas_ABC_Dealer_Block.html",
+	"public/Auctions/Postsale/EDGE_Block_Summary.html",
+	"public/Auctions/Presale/Inventory_Report_By_Seller.html",
+	"public/Auctions/Presale/Inventory_Detail.html",
+	"public/Auctions/Postsale/Buyer_Purchase_Summary_Condensed.html"],
+    "auctions": [
+	"DAANW",
+	"DAAS",
+	"ABCSTL",
+	"RICHAA",
+	"CLCAA",
+	"EPIAA",
+	"ORAA",
+	"STLN",
+	"PLAZA"]
+};
+
